e0fd8b231185bebfa7cd8f22b986c4139bf57ab1
timothyltickle <timothyltickle@localhost>
2013-02-19 18:05:48 -0500

    Working on scripts for access.

--- scripts/manipulateTable.py
+++ scripts/manipulateTable.py
@@ -22,12 +22,29 @@ argp = argparse.ArgumentParser( prog = "manipulateTable.py",
     description = """Performs comman manipulations on tables.""" )
 
 #Arguments
-#For table
-argp.add_argument("-id", dest="sIDName", default="ID", help="Abundance Table ID")
-argp.add_argument("-meta", dest="sLastMetadataName", help="Last metadata name")
-argp.add_argument("-fDelim", dest= "cFileDelimiter", action= "store", default="\t", help="File delimiter, default tab")
-argp.add_argument("-norm", dest="fNormalize", action="store_true", default=False, help="Flag to turn on normalization")
-argp.add_argument("-sum", dest="fSum", action="store_true", default=False, help="Flag to turn on summation")
+#Describe table
+argp.add_argument("--id", dest="sIDName", default="ID", help="Abundance Table ID")
+argp.add_argument("--meta", dest="sLastMetadataName", help="Last metadata name")
+argp.add_argument("--fDelim", dest= "cFileDelimiter", action= "store", default="\t", help="File delimiter, default tab")
+
+argp.add_argument("--doNorm", dest="fNormalize", action="store_true", default=False, help="Flag to turn on normalization")
+argp.add_argument("--doSum", dest="fSum", action="store_true", default=False, help="Flag to turn on summation")
+
+#Unsupervised filtering
+argp.add_argument("--doFilterPercentile", dest="fFilterPercentile", action="store_true", default=False, help="Flag to turn on filtering by percentile (should be performed on a normalized file).")
+argp.add_argument("--doFilterOccurrence", dest="fFilterOccurence", action="store_true", default=False, help="Flag to turn on summation (should NOT be performed on a normalized file).")
+argp.add_argument("--doFilterDeviation", dest="fFilterDeviation", action="store_true", default=False, help="Flag to turn on summation (should NOT be performed on a normalized file).")
+
+#Change bug membership
+argp.add_argument("--makeTerminal", dest="fMakeTerminal", action="store_true", default=False, help="Works reduces the file to teminal features in the original file.")
+argp.add_argument("--reduceOTUs", dest="fRemoveOTUs", action="store", default=None, help="Remove otu entries from file.")
+argp.add_argument("--reduceToClade", dest="iClade", action="store", default=None, help="Specify a level of clade to reduce to [].")
+
+#Manipulate based on metadata
+argp.add_argument("--doPrefixClades", dest="fPrefixClades", action="store_true", default=False, help="Flag to turn on adding prefixes to clades to better identify them, for example s__ will be placed infront of each species.")
+
+
+
 argp.add_argument("strFileAbund", help ="Input data file")
 argp.add_argument("strOutFile", help ="Output file")
 
