6b321ae52557516def6fa24dc20a9ce19195439b
timothyltickle <timothyltickle@localhost>
2013-03-10 01:51:01 -0500

    Feature plotting was added for box, scatter and histogram plots.

--- ScatterPlot.py
+++ ScatterPlot.py
@@ -1,6 +1,6 @@
 """
 Author: Timothy Tickle
-Description: Class to create box plots.
+Description: Class to create scatter plots.
 """
 
 #####################################################################################
@@ -38,17 +38,17 @@ import matplotlib.pyplot as plt
 from pylab import *
 
 #Plots a matrix
-class BoxPlot:
+class ScatterPlot:
 
   @staticmethod
-  def funcPlot(ly, lsLabels, strOutputFigurePath, strTitle = "Title", strXTitle="X Axis", strYTitle="Y Axis", strColor = "#83C8F9", fJitter=False, fInvert=False, fInvertY=False):
+  def funcPlot(lx, ly, strOutputFigurePath, strTitle = "Title", strXTitle="X Axis", strYTitle="Y Axis", strColor = "#83C8F9", fInvert=False):
     """
-    Plot a box plot with optional jittering.
+    Plot a scatter plot.
 
+    :params	lx: List of x values
+    :type:	List of doubles
     :params	ly: List of y values
     :type:	List of doubles
-    :params	lsLabels: List of labels (x tick lables)
-    :type:	List string
     :params	strOutputFigurePath: File path to make figure
     :type:	String file path
     :params	strTitle: Title of figure
@@ -59,11 +59,7 @@ class BoxPlot:
     :type:	String
     :params	strColor: Hex color for the face of the boxplots
     :type:	String
-    :params	fJitter: Indicator of jittering (true) or not (false)
-    :type:	Boolean
     :params	fInvert: Invert colors (true)
-    :type:	Boolean
-    :params	fInvertY: Invert y axis
     :type:	Boolean
     """
 
@@ -98,29 +94,12 @@ class BoxPlot:
     imgSubplot.tick_params(axis='y', colors=objFigureControl.c_strDetailsColorLetter)
     charMarkerEdgeColor = objFigureControl.c_strDetailsColorLetter
 
-    #Make box plot
-    bp = plt.boxplot(x=ly, notch=1, patch_artist=True)
-    for iindex, ldData in enumerate(ly):
-      ldX = None
-      if fJitter:
-        ldX = [float(iindex+1)+ uniform(-.05,.05) for x in xrange(len(ldData))]
-      else:
-        ldX = [float(iindex+1) for x in xrange(len(ldData))]
-      plt.scatter(x=ldX,y=ly[iindex],c=strColor,marker="o",alpha=objFigureControl.c_dAlpha)
-
-    #Color boxes
-    plt.setp(bp['boxes'], color=objFigureControl.c_strDetailsColorLetter, facecolor=strColor, alpha=dAlpha)
-    plt.setp(bp['whiskers'], color=objFigureControl.c_strDetailsColorLetter)
+    #Make scatter plot
+    plt.scatter(x=lx,y=ly,c=strColor,marker="o",alpha=objFigureControl.c_dAlpha)
 
     #Set ticks and title
-    xtickNames = plt.setp(imgSubplot, xticklabels=lsLabels)
     imgSubplot.set_title(strTitle)
     imgSubplot.title.set_color(objFigureControl.c_strDetailsColorLetter)
-
-    #Invert Y axis
-    if fInvertY:
-      ax = plt.gca()
-      ax.set_ylim(ax.get_ylim()[::-1])
 
     #End plot
     #Save to a file
---
+++ scriptPlotFeature.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+"""
+Author: Timothy Tickle
+Description: Peforms common manipulations on tables
+"""
+
+__author__ = "Timothy Tickle"
+__copyright__ = "Copyright 2012"
+__credits__ = ["Timothy Tickle"]
+__license__ = ""
+__version__ = ""
+__maintainer__ = "Timothy Tickle"
+__email__ = "ttickle@sph.harvard.edu"
+__status__ = "Development"
+
+import sys
+import argparse
+import csv
+import os
+from BoxPlot import BoxPlot
+from Histogram import Histogram
+from ScatterPlot import ScatterPlot
+
+def funcPlotBoxPlot(lxVariable1,lxVariable2,fOneIsNumeric):
+
+  ly,lsLabels = [lxVariable1,lxVariable2] if fOneIsNumeric else [lxVariable2,lxVariable1]
+
+  # Group data
+  dictGroups = {}
+  for iIndex in xrange(len(ly)):
+    lsList = dictGroups.get(lsLabels[iIndex],[])
+    lsList.append(ly[iIndex])
+    dictGroups.setdefault(lsLabels[iIndex],lsList)
+  ly = [dictGroups[sKey] for sKey in dictGroups.keys()]
+  lsLabels = dictGroups.keys()
+
+  BoxPlot.funcPlot(ly=ly, lsLabels=lsLabels, strOutputFigurePath=args.strOutputFile, strTitle=args.strTitle, strXTitle=args.strX, strYTitle=args.strY, strColor=args.strColor, fJitter=False, fInvert=args.fColor, fInvertY=args.fAxis)
+
+
+#Set up arguments reader
+argp = argparse.ArgumentParser( prog = "scriptBoxPlot.py\nExample: python scriptBoxPlot.py Input.pcl valuesID groupID",
+    description = "Make a box plot from an abundance table.")
+
+#Sepecify output if needed
+argp.add_argument("-o","--output", dest="strOutputFile", action="store", default=None, help="Output file name.")
+
+# Text annotation
+argp.add_argument("-t","--title", dest="strTitle", action="store", default="Title", help="Test for the title.")
+argp.add_argument("-x","--xaxis", dest="strX", action="store", default="X axis", help="Text for the x-axis.")
+argp.add_argument("-y","--yaxis", dest="strY", action="store", default="Y axis", help="Text for the y-axis.")
+
+# Color options
+argp.add_argument("-c","--color", dest="strColor", action="store", default="#83C8F9", help="Fill color as a Hex number (including the #).")
+argp.add_argument("-r","--invertcolor", dest="fColor", action="store_true", default=False, help="Flag to invert the background to black.")
+
+# Axis adjustments
+argp.add_argument("-s","--invertyaxis", dest="fAxis", action="store_true", default=False, help="Flag to invert the y axis.")
+
+# Required
+argp.add_argument("strFileAbund", help ="Input data file")
+argp.add_argument("strFeatures", nargs = "+", help="Features to plot (from one to two metadata).")
+
+args = argp.parse_args( )
+
+# If the output file is not specified, make it up
+if not args.strOutputFile:
+  lsPieces = os.path.splitext(args.strFileAbund)
+  args.strOutputFile = lsPieces[0]+"-plotfeature.pdf"
+
+csvReader = csv.reader(open(args.strFileAbund, 'rU') if isinstance(args.strFileAbund,str) else args.strFileAbund, delimiter="\t")
+
+#Holds the data
+lxVariable1 = None
+lxVariable2 = None
+fOneIsNumeric = False
+fTwoIsNumeric = False
+
+strFeatureOneID = args.strFeatures[0]
+strFeatureTwoID = None if len(args.strFeatures)<2 else args.strFeatures[1]
+
+# Get values and groupings
+for lsLine in csvReader:
+  if lsLine[0] == strFeatureOneID:
+    lxVariable1 = lsLine[1:]
+  if not strFeatureTwoID is None:
+    if lsLine[0] == strFeatureTwoID:
+      lxVariable2 = lsLine[1:]
+
+# Type variables
+if not lxVariable1 is None:
+  try:
+    float(lxVariable1[0])
+    lxVariable1 = [float(xItem) for xItem in lxVariable1]
+    fOneIsNumeric = True
+  except ValueError:
+    pass
+
+if not lxVariable2 is None:
+  try:
+    float(lxVariable2[0])
+    lxVariable2 = [float(xItem) for xItem in lxVariable2]
+    fTwoIsNumeric = True
+  except ValueError:
+    pass
+
+# Plot as needed
+if((not lxVariable1 is None ) and (not lxVariable2 is None)):
+  if(sum([fOneIsNumeric, fTwoIsNumeric])==0):
+    print "scriptPlotFeature:: Error, If plotting 2 variables, atleast 1 should be numeric."
+  elif(sum([fOneIsNumeric, fTwoIsNumeric])==1):
+    funcPlotBoxPlot(lxVariable1,lxVariable2, fOneIsNumeric=fOneIsNumeric)
+  elif(sum([fOneIsNumeric, fTwoIsNumeric])==2):
+    ScatterPlot.funcPlot(lxVariable1, lxVariable2, args.strOutputFile, strTitle=args.strTitle, strXTitle=args.strX, strYTitle=args.strY, strColor=args.strColor, fInvert=args.fColor)
+elif(not lxVariable1 is None ):
+  if fOneIsNumeric:
+    Histogram.funcPlot(lxVariable1, args.strOutputFile, strTitle=args.strTitle, strXTitle=args.strX, strYTitle=args.strY, strColor=args.strColor, fInvert=args.fColor)
+  else:
+    print "Sorry currently histograms are support for only numeric data."
