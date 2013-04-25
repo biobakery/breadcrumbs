Added the ability to run PCA seperately on all bug data and continuous metadata and add back to the pcl file metametadata and metabugs (named with thier PC component order and percent variance). This uses SVD PCA and not eigen values. Began a parent class Ordination inherit different ordination methods from but not finished with it's integration. Currently integrated into PCA.py and PCoA.py needs to be integrated. Need to move on to other things for now.

scripts/scriptManipulateTable.py
@@ -13,11 +13,15 @@ __maintainer__ = "Timothy Tickle"
 __email__ = "ttickle@sph.harvard.edu"
 __status__ = "Development"
 
+import argparse
 import csv
 import sys
-import argparse
+import re
 import os
+import numpy as np
 from src.AbundanceTable import AbundanceTable
+from src.PCA import PCA
+from src.ValidateData import ValidateData
 
 #Set up arguments reader
 argp = argparse.ArgumentParser( prog = "scriptManipulateTable.py",
@@ -59,10 +63,12 @@ argp.add_argument("-x","--doPrefixClades", dest="fPrefixClades", action="store_t
 #argp.add_argument("-m","--combineIntersect", dest="fCombineIntersect", action="store_true", default=False, help="Combine two tables including only common features/metadata (intersection).")
 #argp.add_argument("-e","--combineUnion", dest="fCombineUnion", action="store_true", default=False, help="Combine two tables (union).")
 
+#Dimensionality Reduction
+argp.add_argument("-p","--doPCA", dest="fDoPCA",action="store_true", default=False, help="Flag to turn on adding metabugs and metametadata by performing PCA on each of bug relative abundance and continuous metadata and add the resulting components")
+
 #Checked
 argp.add_argument("-o","--output", dest="strOutFile", action="store", default=None, help="Indicate output pcl file.")
 argp.add_argument("strFileAbund", help ="Input data file")
-
 
 args = argp.parse_args( )
 
@@ -239,6 +245,48 @@ if args.fPrefixClades:
       print "ManipulateTable::Clade Prefix was added to "+abndTable.funcGetName()
     else:
       print "ManipulateTable::ERROR. Clade Prefix was NOT added to "+abndTable.funcGetName()
+
+# Reduce dimensionality
+if args.fDoPCA:
+  pcaCur = PCA()
+  for abndTable in lsTables:
+
+    # Add data features
+    # Make data components and add to abundance table
+    pcaCur.loadData(abndTable,True)
+    pcaCur.run()
+    ldVariance = pcaCur.getVariance()
+    lldComponents = pcaCur.getComponents()
+    # Make Names
+    lsNamesData = ["Data_PC"+str((tpleVariance[0]+1))+"_"+re.sub("\.","_",str(tpleVariance[1])) for tpleVariance in enumerate(ldVariance)]
+    abndTable.funcAddDataFeature(lsNamesData,lldComponents)
+
+    # Add metadata features
+    # Convert metadata to an input for PCA
+    dictMetadata = abndTable.funcGetMetadataCopy()
+    ## Remove the metadta id
+    dictMetadata.pop(abndTable.funcGetIDMetadataName(),None)
+    lMetadata = []
+    for lxItem in dictMetadata.values():
+      # Replace NAs with the Mode
+      dictFreq = {}
+      for xItem in lxItem:
+        if not xItem.strip().lower() in ["na",""]:
+          dictFreq[xItem] = dictFreq.get(xItem,0)+1
+      xCurMode = max((v, k) for k, v in dictFreq.iteritems())[1]
+      lxItem = [xCurMode if xItem.strip().lower() in ["na",""] else xItem.strip() for xItem in lxItem]
+      ## Get only numeric metadata
+      if sum([ ValidateData.funcIsValidStringFloat(xItem) for xItem in lxItem]) == len(lxItem):
+        lMetadata.append([float(xItem) for xItem in lxItem])
+    pcaCur.loadData(np.array(lMetadata).T,False)
+    pcaCur.run()
+    ldVariance = pcaCur.getVariance()
+    lldComponents = pcaCur.getComponents()
+    # Make Names
+    lsNamesMetadata = ["Metadata_PC"+str((tpleVariance[0]+1))+"_"+re.sub("\.","_",str(tpleVariance[1])) for tpleVariance in enumerate(ldVariance)]
+    # Make metadata components and add to abundance
+    llsMetadata = [list(npdRow) for npdRow in lldComponents]
+    abndTable.funcAddMetadataFeature(lsNamesMetadata, llsMetadata)
 
 #Manipulate based on metadata
 if args.strStratifyBy: