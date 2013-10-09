b313946bdcaa6f1e20c19a70fa4dcaf5c9565480
rschwager-hsph <schwager@hsph.harvard.edu>
2013-10-09 22:00:33 +0000

    accidentally commited a merge conflict

--- setup.py
+++ setup.py
@@ -27,15 +27,11 @@ __maintainer__ = "Timothy Tickle"
 __email__ = "ttickle@sph.harvard.edu"
 __status__ = "Development"
 
-<<<<<<< local
 MIN_SETUPTOOLS = '0.6.28'
 
 import sys
-=======
->>>>>>> other
 import subprocess
 
-<<<<<<< local
 import setuptools
 from distutils.version import StrictVersion
 
@@ -49,54 +45,10 @@ if StrictVersion(setuptools.__version__) < StrictVersion(MIN_SETUPTOOLS):
     subprocess.check_call('pip install --upgrade distribute'.split())
 
     print "Complete. New version: %s" %(setuptools.__version__)
-=======
-from setuptools import setup, find_packages
->>>>>>> other
 
 subprocess.check_call('pip install numpy==1.7.1'.split())
-<<<<<<< local
-=======
 subprocess.check_call('pip install scipy==0.12.0'.split())
->>>>>>> other
 subprocess.check_call('pip install https://github.com/bipy/pyqi/archive/v0.2.0.tar.gz#egg=pyqi-0.2.0'.split())
-<<<<<<< local
-subprocess.check_call('pip install -r requirements.txt'.split())
 cmd = 'python actually_setup.py'.split()
 cmd.extend(arguments)
 subprocess.check_call(cmd)
-=======
->>>>>>> other
-
-<<<<<<< local
-=======
-setup(
-    name='breadcrumbs',
-    author="Timothy Tickle",
-    author_email="ttickle@sph.harvard.edu",
-    license="MIT",
-    version='0.8.0',
-    description='Assorted metagenomics tools',
-    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
-    zip_safe=False,
-    install_requires=[
-        'Cython==0.19.1',
-        'argparse',
-        'cogent==1.5.3',
-        'matplotlib==1.3.0',
-        'numpy==1.7.1',
-        'scipy==0.12.0',
->>>>>>> other
-
-<<<<<<< local
-=======
-        'pyqi==0.2.0',
-        'biom-format==1.2.0',
-    ],
-    dependency_links=[
-        'https://github.com/biom-format/biom-format/archive/v1.2.0.tar.gz#egg=biom-format-1.2.0',
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta"
-    ],
-)
->>>>>>> other
